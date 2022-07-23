727198f8ff445f7849c16369892891bced1c5c1d
Scarlet-Jo <100141355+Scarlet-Jo@users.noreply.github.com>
2022-07-23 19:11:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -78,11 +78,12 @@ def a(client, message):
             duration = results[0]["duration"]
             katy = "[Music Bot](https://t.me/all_music_helpbot)"
             ids = "message.from_user.username"
+            performer=f"@All_Music_Grp"
 
             #UNCOMMENT THIS IF YOU WANT A LIMIT ON DURATION. CHANGE 1800 TO YOUR OWN PREFFERED DURATION AND EDIT THE MESSAGE (30 minutes cap) LIMIT IN SECONDS
-            if time_to_seconds(duration) >= 1800:  # duration limit
-                m.edit("Exceeded 30mins cap")
-                return
+            #if time_to_seconds(duration) >= 1800:  # duration limit
+                #m.edit("Exceeded 30mins cap")
+                #return
 
             views = results[0]["views"]
             thumb_name = f'thumb{message.message_id}.jpg'
